2eb133bda1024f5d7b126d235869172fea2e19c7
Vega Facundo <vegafacundo187@gmail.com>
2023-04-14 20:02:44 -0300

    validate tg mp

---
+++ src/utils/globalConst.js
@@ -0,0 +1,4 @@
+export const newFiumbiFormClientid = '8610819450989194'
+export const newFiumbiFormRedirect_uri =
+  'http://localhost:5173/validatingFiumbiUser'
+export const newFiumbiFormScope = 'read'
