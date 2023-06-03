a78ea73f34f13bb1eef73dac7c8c6087e1dc8080
Facu Quero <facuquero@gmail.com>
2023-06-03 17:54:33 -0300

    Update TermsConditions.jsx

--- src/routes/TermsConditions.jsx
+++ src/routes/TermsConditions.jsx
@@ -12,11 +12,89 @@ const TermsConditions = () => {
     >
       <Grid item xs={11} sm={8}>
         <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
-          titulo
+          Terminos y Condiciones
         </Typography>
       </Grid>
       <Grid item xs={11} sm={8} my={2} pl={2}>
-        <Typography>descripcion</Typography>
+        <Typography>
+          Definiciones:
+          
+          Usuario:
+Se define como "Usuario" a toda persona, tanto física como jurídica, registrada dentro de Fiumbi. En el caso de personas jurídicas, deberán ser registradas en nombre de un representante, quien será responsable de recibir los productos en el domicilio proporcionado.
+
+Cliente:
+Se define como "Cliente" a la persona o entidad responsable de pagar a los Usuarios por los productos en su lista. El Cliente utiliza los servicios de Fiumbi para facilitar la transacción entre los Usuarios y los Proveedores.
+
+Proveedores:
+Se define como "Proveedores" a las tiendas o establecimientos que publican sus productos en una plataforma ajena. 
+
+Servicio:
+Se define como "Servicio" a la plataforma y funcionalidades ofrecidas por Fiumbi. El Servicio tiene como objetivo garantizar la generación de la transacción del producto seleccionado por el Cliente desde la lista del Usuario. Fiumbi actúa como intermediario para facilitar la transacción entre los Usuarios y los Proveedores, permitiendo que los Usuarios reciban los productos seleccionados.
+
+
+<strong>No afiliación con MercadoLibre</strong>
+Fiumbi desea dejar en claro que no tenemos ninguna relación, asociación o afiliación con MercadoLibre. No tenemos ningún contrato ni acuerdo con MercadoLibre, ni utilizamos su marca de manera indebida con fines lucrativos.
+
+La mención de MercadoLibre en el contexto de nuestros servicios se realiza únicamente con fines descriptivos para proporcionar información a los Usuarios y a los Clientes sobre los productos ofrecidos por los Proveedores. Cualquier referencia a MercadoLibre es meramente informativa y no implica que Fiumbi esté respaldado o asociado con MercadoLibre de ninguna manera.
+
+Fiumbi se compromete a respetar los derechos de propiedad intelectual de terceros, incluyendo los derechos de marca registrada y otros derechos de propiedad de MercadoLibre. Nos esforzamos por garantizar que cualquier uso de marcas comerciales, logotipos o material protegido por derechos de autor se realice de conformidad con las leyes aplicables y dentro de los límites permitidos por la legislación vigente.
+
+Entendemos la importancia de proteger los derechos de propiedad intelectual y nos comprometemos a no utilizar la marca de MercadoLibre de manera indebida o con fines comerciales no autorizados. En caso de que se detecte alguna infracción o uso no autorizado de la marca de MercadoLibre en nuestro sitio web, nos comprometemos a tomar medidas inmediatas para corregir cualquier problema y evitar problemas legales o de otra índole.
+
+Al utilizar nuestros servicios, los Usuarios y los Clientes aceptan que Fiumbi no tiene ninguna relación comercial con MercadoLibre y reconocen que cualquier referencia a MercadoLibre se realiza únicamente con fines informativos y descriptivos.
+
+<strong>OBJETO:</strong>
+Los presentes términos y condiciones tienen por objeto regular la totalidad de la relación jurídica que surge de la contratación que realiza el Cliente del Servicio ofrecido por Fiumbi. Estas condiciones constituyen un contrato de adhesión en los términos del artículo 38 de la Ley 24.240 y se encuentran publicadas en la página web de Fiumbi (https://www.fiumbi.com/terminos-y-condiciones).
+
+El objeto de este contrato es establecer los términos y condiciones para el uso y acceso al Servicio proporcionado por Fiumbi. El Servicio tiene como finalidad garantizar la generación de la transacción del producto seleccionado por el Cliente desde la lista del Usuario, actuando como intermediario entre los Usuarios y los Proveedores.
+
+Fiumbi no realiza los envíos, no ofrece los productos y no se hace responsable del estado del producto. Nos comprometemos a generar la transacción en otra plataforma externa que será la responsable del estado del producto y de su envío. Cualquier información sobre el estado del producto, políticas de reembolso u otras condiciones relacionadas con la adquisición serán proporcionadas por dicha plataforma externa.
+
+Al aceptar estos términos y condiciones, el Cliente reconoce y entiende que Fiumbi no tiene control ni responsabilidad sobre los productos ofrecidos por los Proveedores ni sobre la plataforma externa encargada del estado del producto y envío. El Cliente reconoce que cualquier reclamo, disputa o inconveniente relacionado con el producto o la transacción deberá ser dirigido a la plataforma externa designada.
+
+<strong>Perfección del contrato:</strong>
+Acorde a lo estipulado por los artículos 1105 y 1106, el presente es un contrato a distancia. El mismo quedará perfeccionado luego de completar el correspondiente formulario de contratación que se ofrece en el sitio web de Fiumbi (https://www.fiumbi.com/) y desde el momento en que el Cliente presiona el botón "Regalar ahora", previa lectura y conformidad a estas condiciones de contratación y política de privacidad, las cuales le son proporcionadas de manera inmediatamente previa a dicho botón de "Regalar ahora".
+
+Las partes, de común acuerdo, aceptan equiparar la firma autógrafa del Cliente a la realizada mediante cualquier tipo de clave, código o elemento de seguridad identificativo utilizado y conocido solo por el Cliente y/o puesto a disposición por la Empresa.
+
+El botón "Regalar ahora" es el medio mediante el cual el Cliente manifiesta su voluntad de contratar los servicios ofrecidos por Fiumbi, comprometiéndose a cumplir con los términos y condiciones establecidos en este contrato.
+
+Es importante destacar que la acción de presionar el botón "Regalar ahora" constituye una aceptación expresa por parte del Cliente de todas las disposiciones contenidas en estos términos y condiciones, así como de la política de privacidad aplicable a los servicios proporcionados por Fiumbi.
+
+<Typography variant="h5" sx={{ fontWeight: 'bold' }}>
+          Politicas de privacidad
+        </Typography>
+¿Cómo tratamos sus datos de manera segura, responsable y confidencial?
+
+En esta Política de Privacidad (la Política, en adelante) se describen todos los derechos y obligaciones de las partes al utilizar el servicio del sitio web "https://fiumbi.com/".
+
+Es obligación del Cliente leer y aceptar esta Política, que forma parte de las "Condiciones Generales de Contratación" ("https://fiumbi.com/terminos-y-condiciones"), previo a proceder la utilización de los servicios proporcionados por la Empresa. En caso de no estar de acuerdo con alguna/s de las cláusulas insertas, deberá abstenerse de la utilización de los servicios que brinde o pudiere brindar en un futuro la Empresa. La utilización del servicio implica la aceptación por parte del Cliente de la presente Política y Condiciones Generales de Contratación.
+
+A todo efecto, se entiende por:
+Sitio: https://fiumbi.com/
+Política: Así se abrevia la presente "Política de Privacidad" del Sitio.
+
+CGC: Son las "Condiciones Generales de Contratación", las cuales rigen toda la relación contractual establecida entre el Cliente y la Empresa.
+
+AAIP: Agencia de Acceso a la Información Pública - Organismo de contralor.
+
+Propiedad Intelectual : Fiumbi reconoce y respeta los derechos de propiedad intelectual relacionados con los productos. No se permite la reproducción, distribución o uso no autorizado de logotipos, marcas comerciales u otro contenido protegido sin la autorización previa y expresa del titular de los derechos.
+
+Confidencialidad : Fiumbi se compromete a mantener la confidencialidad de la información proporcionada por los clientes durante el proceso de registro del Usuario. No se divulgará ni se utilizará dicha información con fines distintos a los necesarios para completar la transacción de manera segura y eficiente.
+En ninguna situación se revelará información personal de los Usuarios hacia terceros. 
+Uso Adecuado : El servicio adquiridos a través de "https://fiumbi.com/" deben ser utilizadas únicamente con fines legales y legítimos. Queda estrictamente prohibido promover o facilitar el uso fraudulento o ilegal del servicio
+
+Información y Responsabilidad : "https://fiumbi.com/" se compromete a proporcionar información precisa y actualizada sobre los requisitos y procedimientos necesarios para que un Usuario pueda recibir un producto financiado por un Cliente.
+
+
+Ley Aplicable y Jurisdicción : Estos Términos se regirán e interpretarán de acuerdo con las leyes de la República Argentina. Cualquier disputa o controversia que surja en relación el servicio estará sujeta a la jurisdicción exclusiva de los tribunales competentes de la República Argentina.
+
+
+
+ALOJAMIENTO WEB
+Nuestros servidores se encuentran en el ámbito de la República Argentina, Estados Unidos de América y/o dentro del espacio de la Unión Europea, siendo la Unión Europea considerada como un espacio con legislación adecuada en materia de protección de datos personales y permitida la transferencia de datos. Todo de conformidad con el art. 3 de la Disposición 60 – E/2016 de la Dirección Nacional de Protección de Datos y el art. 12 de la Ley 25.326. Ellos cuentan con medidas de seguridad técnica
+
+       </Typography>
       </Grid>
     </Grid>
   )
